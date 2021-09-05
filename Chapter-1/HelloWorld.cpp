25e3ed74284785077868060191364f00504b8ab9
Alberto A. Colón Reyes <alberto_colon@me.com>
2021-09-04 23:45:55 -0400

    Adding the first code from the Book Starting out with CPP

---
+++ Chapter-1/HelloWorld.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string name = "Alberto A. Colón";
+
+    cout << "Hello World!\n\tMy name is " << name << endl;
+    cout << "                                    \n";
+    cout << "-[-[-[-[-[-[-[-[-[-[-[-[-[-[-[-[-\n";
+    cout << "                                     \n";
+    cout << "Glad that finally I can start practicing more code";
+    return 0;
+
+}
+
