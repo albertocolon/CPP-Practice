3011f5ccfa54cd3d3488ef4fbf6070e5cc0253c1
Alberto A. Colón Reyes <alberto_colon@me.com>
2021-09-07 17:25:15 -0400

    Update menuWithFunctions.cpp
    Just adding more code to the file adding the methods and explanations.

--- Chapter-6/menuWithFunctions.cpp
+++ Chapter-6/menuWithFunctions.cpp
@@ -7,3 +7,38 @@
 using namespace std;
 
 // Function prototypes
+void showMenu();
+void showFees(double, int);
+
+int main()
+{
+    int choice;   // To hold a menu choice.
+    int months;   // To hold a number of months.
+
+    // Constants for the menu choices
+    const int ADULT_CHOICE = 1, CHILD_CHOICE = 2, SENIOR_CHOICE = 3, QUIT_CHOICE = 4;
+
+    // Constants for membership rates.
+    const double ADULT = 40.00, CHILD = 20.00, SENIOR = 30.00;
+
+
+    // Set up numeric output formating.
+    cout << fixed << showpoint << setprecision(2);
+
+    do
+    {
+        // Display the menu and get the user's choice.
+        showMenu();
+        cin >> choice;
+
+        // Validate the menu selection.
+     while (choice < ADULT_CHOICE || choice > QUIT_CHOICE)
+        {
+        cout << "Please enter a valid menu choice: ";
+        cin >> choice;
+
+        }
+    } 
+
+
+}
