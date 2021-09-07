b4262c2165c36510f7b2932f9fcf0486740385e2
Alberto A. Colón Reyes <alberto_colon@me.com>
2021-09-07 13:19:09 -0400

    Practicing program 6-10 from the book

---
+++ Chapter-6/menuWithFunctions.cpp
@@ -0,0 +1,9 @@
+/* This is a menu-driven program that makes a function call 
+* for each selection the use makes. 
+*/
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+// Function prototypes
