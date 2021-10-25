43f99eec950d90ace34e364f9d7f08443eff8428
Alberto A. Colón Reyes <alberto_colon@me.com>
2021-10-25 11:43:14 -0400

    Create pointerStructAge.cpp
    Adding the struct for age problem

---
+++ Chapter-9/pointerStructAge.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <tring>
+
+struct CurrentDate {
+    int cYear;
+    int cMonth;
+    int cDay;
+};
+
+struct BirthDate {
+    int bYear;
+    int bMonth;
+    int bDay;
+}
+
