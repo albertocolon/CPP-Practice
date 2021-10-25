8a4ff5281a0a70cef1581a9fa57aa2a7576e2a73
Alberto A. Colón Reyes <alberto_colon@me.com>
2021-10-25 12:00:33 -0400

    Continue the failed final exam problem 3

--- Chapter-9/pointerStructAge.cpp
+++ Chapter-9/pointerStructAge.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-#include <tring>
+#include <string>
+using namespace std;
 
 struct CurrentDate {
     int cYear;
@@ -11,5 +12,31 @@ struct BirthDate {
     int bYear;
     int bMonth;
     int bDay;
+};
+
+void greeting();
+
+int main()
+{
+    CurrentDate today;
+    string person;
+    int *ptr;
+    
+    greeting();
+
+
+    return 0;
 }
 
+
+void greeting()
+{
+    string name;
+    cout << "\t\tWelcome!\n";
+    cout << "\tI'll try to guess your age.\n";
+    cout << "\tPlease tell me your name: ";
+    cin >> name; 
+    cout << endl;
+    cout << "Thanks " << name << endl;
+
+}
